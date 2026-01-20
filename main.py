cabefd5f2d016053612fb6946d2bf021528b8c52
NeoWang <Wangzining@zju.edu.cn>
2026-01-21 00:05:00 +0800

    modified the method of parameter passing

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import matplotlib.pyplot as plt
 
 #create a basic Beam class
 class Beam:
-    def __init__(self, range=1e-3, resolution=1024):
+    def __init__(self, range=1e-3, resolution=1024, z=0., phi=0.):
         self.range = range
         self.resolution = resolution
+        self.z = z
+        self.phi = phi
         pass
 
-    def rasterized_Beam(self, z=0., phi=0.):
+    def rasterized_Beam(self):
         x = np.linspace(-self.range, self.range, self.resolution)
         y = np.linspace(-self.range, self.range, self.resolution)
         X, Y = np.meshgrid(x, y)
 
-        Beam = ((X + 1j * Y) / (X + 1j * Y)) * (z**0)
+        Beam = ((X + 1j * Y) / (X + 1j * Y)) * (self.z**0)
 
         return Beam
     
-    def plot_intensity(self, z=0., phi=0.):
-        self.intensity = np.abs(self.rasterized_Beam(z=z, phi=phi))**2
+    def plot_intensity(self):
+        self.intensity = np.abs(self.rasterized_Beam())**2
 
         plt.imshow(self.intensity, extent=(-self.range, self.range, -self.range, self.range), cmap='inferno')
         plt.colorbar(label='Intensity', extend='max')
@@ -28,8 +30,8 @@ class Beam:
 
         plt.show()
 
-    def plot_phase(self, z=0., phi=0.):
-        self.angle = np.angle(self.rasterized_Beam(z=z,phi=phi))%(2 * np.pi)
+    def plot_phase(self):
+        self.angle = np.angle(self.rasterized_Beam())%(2 * np.pi)
 
         plt.imshow(self.angle, extent=(-self.range, self.range, -self.range, self.range), cmap='gray')
         plt.colorbar(label='Phase')
@@ -42,8 +44,8 @@ class Beam:
 
 #create the G_Beam class
 class G_Beam(Beam):
-    def __init__(self, range=1e-3, resolution=1024, w0=1e-3, lam=632.8e-9):
-        super().__init__(range, resolution)
+    def __init__(self, range=1e-3, resolution=1024, z=0., phi=0., w0=1e-3, lam=632.8e-9):
+        super().__init__(range, resolution, z, phi)
 
         self.w0 = w0
         self.lam = lam
@@ -53,21 +55,21 @@ class G_Beam(Beam):
         self.Beam = self.rasterized_Beam()
         pass
     
-    def rasterized_Beam(self, z=0., phi=0.):
+    def rasterized_Beam(self):
         x = np.linspace(-self.range, self.range, self.resolution)
         y = np.linspace(-self.range, self.range, self.resolution)
         X, Y = np.meshgrid(x, y)
 
         if z==0:
-            Beam = np.exp(-(X**2 + Y**2) / self.w0**2) * np.exp(1j*(self.k * z - phi))
+            Beam = np.exp(-(X**2 + Y**2) / self.w0**2) * np.exp(1j*(self.k * self.z - phi))
         elif z>0:
-            R = self.z_R * ((z / self.z_R) + (self.z_R / z))
+            R = self.z_R * ((self.z / self.z_R) + (self.z_R / self.z))
         
-            w = self.w0 * np.sqrt(1 + (z / self.z_R)**2)
+            w = self.w0 * np.sqrt(1 + (self.z / self.z_R)**2)
 
-            Beam = (self.w0 / w) * np.exp(-(X**2 + Y**2) / w**2) * np.exp(1j * (self.k * (X**2 + Y**2) / (2 * R) - phi + self.k * z))
+            Beam = (self.w0 / w) * np.exp(-(X**2 + Y**2) / w**2) * np.exp(1j * (self.k * (X**2 + Y**2) / (2 * R) - phi + self.k * self.z))
         else:
-            Beam = ((X + 1j * Y) / (X + 1j * Y)) * (z**0)
+            Beam = ((X + 1j * Y) / (X + 1j * Y)) * (self.z**0)
 
         return Beam
 
@@ -80,8 +82,8 @@ if __name__ == "__main__":
     lam=632.8e-9
     z=0.
     phi=0.
-    Beam = G_Beam(range=range, resolution=resolution, w0=w0, lam=lam)
-    Beam.rasterized_Beam(z=z, phi=phi)
-    Beam.plot_intensity(z=z, phi=phi)
-    Beam.plot_phase(z=z, phi=phi)
+    Beam = G_Beam(range=range, resolution=resolution, z=z, phi=phi, w0=w0, lam=lam)
+    Beam.rasterized_Beam()
+    Beam.plot_intensity()
+    Beam.plot_phase()
     pass
